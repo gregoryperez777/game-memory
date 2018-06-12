9f129fcaa562f69f33cd4899dbc9058449507c0d
Gregory Perez <gregoryperezfigueredo@gmail.com>
2018-06-11 20:46:58 -0400

    + Se desarrollo el component Card y ademas se añadio al component Board

--- src/Board.js
+++ src/Board.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
+import Card from './Card';
 import './Board.css';
 
 export default class Board extends Component {
 	render() {
-		const cards = [1,2,3,4,5,6];
+		const cards = [1,2,3,4,5,6,7,8,9,10];
 
 		return (
-			<div clasName="board">
+			<div className="board">
 				{
-					cards.map((card) => <span>{ card }</span>) 
+					cards.map((card) => <Card></Card>) 
 				}
 			</div>	
 		);
 	}
-} 
+}
---
+++ src/Card.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './Card.css';
+
+export default class Card extends Component {
+	render() {
+		return (
+			<div className="card">
+			</div>	
+		);
+	}
+}
